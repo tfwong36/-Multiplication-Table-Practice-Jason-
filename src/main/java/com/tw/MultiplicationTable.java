1cc61d418f9186cafaafb9ee20a5ce9bc6c9b48b
tfwong36 <tfwong36@gmail.com>
2021-11-30 16:06:59 +0800

    [multiplication-table-practise][Jason]feat:Implement MultiplicationTableTest.java

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,30 +2,44 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int start, int end) {
-        return null;
+        return isValid(start,end)? generateTable(start, end) : null;
     }
 
     public Boolean isValid(int start, int end) {
-        return null;
+        return isInRange(start) && isInRange(end) && isStartNotBiggerThanEnd(start, end);
     }
 
     public Boolean isInRange(int number) {
-        return null;
+        return number >= 1 && number <= 1000;
     }
 
     public Boolean isStartNotBiggerThanEnd(int start, int end) {
-        return null;
+        return start <= end;
     }
 
     public String generateTable(int start, int end) {
-        return null;
+        String output_string = "";
+        for (int i = start; i <= end; i++) {
+            if (i == end)
+                output_string += generateLine(start, i);
+            else
+                output_string += generateLine(start, i) + "\r\n";
+        }
+        return output_string;
     }
 
     public String generateLine(int start, int row) {
-        return null;
+        String output_string = "";
+        for (int i = start; i <= row; i++){
+            if ( i == row )
+                output_string += generateSingleExpression(i, row);
+            else
+                output_string += generateSingleExpression(i, row) + "  ";
+        }
+        return output_string;
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand + "*" + multiplier + "=" + multiplicand * multiplier;
     }
 }
